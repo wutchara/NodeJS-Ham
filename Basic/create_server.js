0f0a2455d1dc94f86585839ff9a8f5f0fa722fa6
wutchara02 <sstmuham@gmail.com>
2017-01-29 17:04:04 +0700

    fix create server.js

--- Basic/create_server.js
+++ Basic/create_server.js
@@ -11,7 +11,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(8081, '127.0.0.1');
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
