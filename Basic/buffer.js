e8cf8f5c5eaacc8a6b0504d900ef90cef3299bfc
wutchara02 <sstmuham@gmail.com>
2017-02-08 07:15:44 +0700

    buffer

---
+++ Basic/buffer.js
@@ -0,0 +1,13 @@
+var buffer = new Buffer(10);
+var bufLength = buffer.write("HAM");
+var buffer2 = new Buffer([10]);
+var buffer3 = new Buffer("wutchara");
+var buffer4 = new Buffer("wutchara", "utf8");
+
+
+console.log(buffer);
+console.log("Length : " + bufLength);
+console.log(buffer2);
+console.log(buffer3);
+console.log(buffer3.toString());
+console.log(buffer4.toString());
